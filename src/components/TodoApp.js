306922177988b78a3f1103c45594a7d818d04021
nischal08 <nischal.karanjit@gmail.com>
2019-03-12 21:02:06 +0545

    navbar and TodoApp modified

--- src/components/TodoApp.js
+++ src/components/TodoApp.js
@@ -1,14 +1,16 @@
 import React, { Component } from 'react';
 import './TodoApp.css';
+import 'materialize-css/dist/css/materialize.min.css';
+import Navbar from './Navbar';
 
 class TodoApp extends Component {
-  render() {
-    return (
-      <div className="App">
-        <h1>Todo App</h1>
-      </div>
-    );
-  }
+	render() {
+		return (
+			<div className="App">
+				<Navbar />
+			</div>
+		);
+	}
 }
 
 export default TodoApp;
