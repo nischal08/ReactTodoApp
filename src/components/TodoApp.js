36177b5b1f50329e5ead03199f7ed44c8244763a
nischal08 <nischal.karanjit@gmail.com>
2019-03-13 09:27:37 +0800

    display list of todos

--- src/components/TodoApp.js
+++ src/components/TodoApp.js
@@ -2,12 +2,20 @@ import React, { Component } from 'react';
 import './TodoApp.css';
 import 'materialize-css/dist/css/materialize.min.css';
 import Navbar from './Navbar';
+import TodoList from './TodoList';
 
 class TodoApp extends Component {
+	state = {
+		todos: [ { id: 1, content: 'Learn react js' }, { id: 2, content: 'watch movie' } ]
+	};
 	render() {
+		const {todos} = this.state;
 		return (
 			<div className="App">
 				<Navbar />
+				<div className="container">
+					<TodoList todos={todos} />
+				</div>
 			</div>
 		);
 	}
